5660f211ae6b90748453bb05d5e91ce4e89c24fb
Pete Jeffryes <pjeffryes@tendrilinc.com>
2019-01-19 19:03:55 -0700

    add ping route

--- src/server/app.js
+++ src/server/app.js
@@ -19,6 +19,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // *** register routes *** //
+app.use('/ping/', routes.ping);
 app.use('/auth/', routes.auth);
 app.use('/api/', routes.notes);
 
