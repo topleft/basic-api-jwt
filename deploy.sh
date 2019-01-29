2aec415420869c52169a242d6b41825214f8acf7
Pete Jeffryes <pjeffryes@tendrilinc.com>
2019-01-28 20:47:05 -0700

    update deplot script to run locally

--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,22 @@
 
 git checkout origin master
 git pull origin master
-sudo docker build -f Dockerfile.test -t topleft/api-boiler-test .
-CONTAINER_ID=$(sudo docker run -d --env-file=env_file.test topleft/api-boiler-test:latest)
+docker build -f Dockerfile.test -t topleft/api-boiler-test .
+CONTAINER_ID=$(docker run -d --env-file=env_file.test topleft/api-boiler-test:latest)
 echo "Container ID: $CONTAINER_ID"
 EXIT_CODE=$(docker inspect $CONTAINER_ID --format='{{.State.ExitCode}}')
 if [ $EXIT_CODE -eq 0 ]
   then
     echo "Tests past!\n"
-    sudo docker build -f Dockerfile -t topleft/api-boiler:latest .
-    echo build successful!\\n
+    docker build -f Dockerfile -t topleft/api-boiler:latest .
+    echo build succgssessful!\\n
     VERSION=$(npm version patch)
-    sudo git push origin master
-    echo $docker_password | sudo docker login --username topleft --password-stdin
-    sudo docker push topleft/api-boiler:$VERSION
+    git push origin master
+    echo $docker_password | docker login --username topleft --password-stdin
+    docker push topleft/api-boiler:$VERSION
     echo "image: topleft/api-boiler:$VERSION"
     echo push successful!\\n
-    sudo docker run -d -it -p 3030:3030 --env-file=~/env_file.dev topleft/api-boiler:$VERSION
+    docker run -d -it -p 3030:3030 --env-file=env_file.dev topleft/api-boiler:$VERSION
   else
     echo "Tests failed with exit code: $EXIT_CODE"
 fi
