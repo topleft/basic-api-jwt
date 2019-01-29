117c98f0f62b6f6049976a7da7766786b13349d0
Pete Jeffryes <pjeffryes@tendrilinc.com>
2019-01-28 21:19:26 -0700

    update deplot script to run locally

--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,37 @@
 
-git checkout origin master
-git pull origin master
+
+# CICD on merge to master
+
+# 1. run the tests in the docker container
+# 2. if tests pass build container, else exit
+# 3. update application version
+# 4. commit version update
+# 5. tag
+
+# build and run test container
 docker build -f Dockerfile.test -t topleft/api-boiler-test .
 CONTAINER_ID=$(docker run -d --env-file=env_file.test topleft/api-boiler-test:latest)
 echo "Container ID: $CONTAINER_ID"
 EXIT_CODE=$(docker inspect $CONTAINER_ID --format='{{.State.ExitCode}}')
+
 if [ $EXIT_CODE -eq 0 ]
   then
     echo "Tests past!\n"
-    docker build -f Dockerfile -t topleft/api-boiler:latest .
-    echo build succgssessful!\\n
+
+    # bump minor version
     VERSION=$(npm version patch)
     git push origin master
+
+    # build container, tag it and push it to Docker Hub
+    docker build -f Dockerfile -t topleft/api-boiler:$VERSION .
+    docker tag topleft/api-boiler:latest topleft/api-boiler:$VERSION
     echo $docker_password | docker login --username topleft --password-stdin
+    docker push topleft/api-boiler:latestx
     docker push topleft/api-boiler:$VERSION
-    echo "image: topleft/api-boiler:$VERSION"
-    echo push successful!\\n
-    docker run -d -it -p 3030:3030 --env-file=env_file.dev topleft/api-boiler:$VERSION
+
+    echo "docker image: topleft/api-boiler:$VERSION"
+
+    echo "push successful!\n"
   else
     echo "Tests failed with exit code: $EXIT_CODE"
 fi
