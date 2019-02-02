26cb7b3d0504e545e9645ae0a81aeaefafa83f98
Pete Jeffryes <pjeffryes@tendrilinc.com>
2019-02-02 14:02:31 -0700

    getting travis and docker to play nice

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 echo 'Running tests ... '
 docker build -f Dockerfile.test --pull --cache-from topleft/api-boiler-test: -t topleft/api-boiler-test .
-CONTAINER_ID=$(docker run -d -e BASIC_DB_TEST -e TOKEN_SECRET -e NODE_ENV topleft/api-boiler-test:latest)
+CONTAINER_ID=$(docker run -e BASIC_DB_TEST -e TOKEN_SECRET -e NODE_ENV topleft/api-boiler-test:latest)
 echo "Container ID: $CONTAINER_ID"
 
 STATUS=$(docker inspect $CONTAINER_ID --format='{{.State.Status}}')
@@ -13,8 +13,7 @@ EXIT_CODE=$(docker inspect $CONTAINER_ID --format='{{.State.ExitCode}}')
 if [ $EXIT_CODE -eq 0 ];
   then
     echo "\nTests past!\n"
-    exit 0
   else
     echo "\nTests failed\n"
-    exit 1
 fi
+exit EXIT_CODE
