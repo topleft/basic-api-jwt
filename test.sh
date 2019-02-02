10bd2bebd4f1261c20697bc11666902dd51106e7
Pete Jeffryes <pjeffryes@tendrilinc.com>
2019-02-02 14:50:42 -0700

    fix test docker run command

--- test.sh
+++ test.sh
@@ -1,7 +1,9 @@
 echo 'Running tests ... '
-docker build -f Dockerfile.test --pull --cache-from topleft/api-boiler-test -t topleft/api-boiler-test .
-CONTAINER_ID=$(docker run BASIC_DB_TEST=postgres://postgres@host.docker.internal:5432/basic_db_test -e TOKEN_SECRET -e NODE_ENV topleft/api-boiler-test:latest)
+docker build -f Dockerfile.test --pull --cache-from topleft/api-boiler-test:latest -t topleft/api-boiler-test:latest .
+CONTAINER_ID=$(docker run -e BASIC_DB_TEST=postgres://postgres@host.docker.internal:5432/basic_db_test -e TOKEN_SECRET -e NODE_ENV topleft/api-boiler-test:latest)
 echo "Container ID: $CONTAINER_ID"
+
+if ! [ $CONTAINER_ID ]; then exit 1;
 
 STATUS=$(docker inspect $CONTAINER_ID --format='{{.State.Status}}')
 until [ $STATUS = 'exited' ]; do
