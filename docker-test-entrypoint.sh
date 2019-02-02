37d6b64bf23a079b2ed418e7f0dbdb92fbbf3371
Pete Jeffryes <pjeffryes@tendrilinc.com>
2019-02-02 15:04:45 -0700

    working it out

--- docker-test-entrypoint.sh
+++ docker-test-entrypoint.sh
@@ -1,2 +1,2 @@
-npm run knex -- migrate:latest --env test --knexfile ./knexfile.js
+BASIC_DB_TEST=postgres://localhost:5432/basic_db_test npm run knex -- migrate:latest --env test --knexfile ./knexfile.js
 npm run test
