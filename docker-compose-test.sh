c389f438478ecfdc2b6c39355f6fe06f58bebcb4
Pete Jeffryes <pjeffryes@tendrilinc.com>
2019-02-09 20:31:55 -0700

    retry logic in test script

--- docker-compose-test.sh
+++ docker-compose-test.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
+echo "Trying migration"
 
-docker-compose run web sh migrate.sh
-until [ $? -eq 0 ]; do
-  docker-compose run web sh migrate.sh
+RETRIES=5
+until docker-compose run web sh migrate.sh > /dev/null 2>&1; do
+  echo "Waiting for postgres server, $((RETRIES--)) remaining attempts..."
+  sleep 5
+  if [ $RETRIES -eq 0 ]
+  then
+    exit 1
+  fi
 done
+echo "migration successful!"
+
 docker-compose run web npm run test
