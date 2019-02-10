b2f0bd5ccd2c2ef98adee067789775955816df20
Pete Jeffryes <pjeffryes@tendrilinc.com>
2019-02-09 20:39:08 -0700

    dif bash syntax

--- docker-compose-test.sh
+++ docker-compose-test.sh
@@ -2,8 +2,9 @@
 echo "Trying migration"
 
 RETRIES=5
-until docker-compose run web sh migrate.sh > /dev/null 2>&1; do
-  echo "Waiting for postgres server, $((RETRIES--)) remaining attempts..."
+while docker-compose run web sh migrate.sh > /dev/null 2>&1; do
+  RETRIES=$((RETRIES - 1))
+  echo "Waiting for postgres server, $RETRIES remaining attempts..."
   sleep 5
   if [ $RETRIES -eq 0 ]
   then
